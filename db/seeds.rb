273dcdc013ec7abfef5cf443f3158fd5d57ff185
Desiree Lerma <desi.lerma96@yahoo.com>
2020-12-14 20:30:55 -0600

    Updates seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.create(email: "justin@test.com", username: "Justin", password: "justin123")
 Gift.create(title: "Blu Ray Movie", description: "The Crow with Brandon Lee", user_id: 1)
 Gift.create(title: "Workout Equipment", description: "Cable machine", user_id: 1)
+Gift.create(title: "Shoes", description: "Air Force 1", user_id: 1)
 
 User.create(email: "benson@test.com", username: "Benson", password: "benson123")
 Gift.create(title: "Toy", description: "Squeaky toy", user_id: 2)
@@ -8,6 +9,8 @@ Gift.create(title: "Treats", description: "Doggie treats", user_id: 2)
 
 User.create(email: "desi@test.com", username: "Desi", password: "desi123")
 Gift.create(title: "Gift Card", description: "To go to the movies", user_id: 3)
-Gift.create(title: "Work Out Clothes", description: "Joggers in the color black", user_id: 3)
+Gift.create(title: "Gift Card", description: "To go to the movies", user_id: 3)
 
-# run after deleting schema & dev.sqlite
+
+User.create(email: "thanos@test.com", username: "Thanos", password: "thanos123")
+Gift.create(title: "Infinity Stone", description: "Soul stone", user_id: 4)
